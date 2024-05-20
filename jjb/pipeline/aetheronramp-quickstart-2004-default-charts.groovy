f54fc7a8e3f2bad8b483119d3a6884f4616d9b27
kunal14kapoor <kunal@opennetworking.org>
2024-05-20 09:32:22 +0000

    updated pipeline

--- jjb/pipeline/aetheronramp-quickstart-2004-default-charts.groovy
+++ jjb/pipeline/aetheronramp-quickstart-2004-default-charts.groovy
@@ -1,107 +1,109 @@
-// SPDX-FileCopyrightText: 2023 Open Networking Foundation <info@opennetworking.org>
+// SPDX-FileCopyrightText: 2021 Open Networking Foundation <info@opennetworking.org>
 // SPDX-License-Identifier: LicenseRef-ONF-Member-Only-1.0
 pipeline {
 
   agent {
-        //label 'ubuntu-18.04-t3a2xlarge-mumbai'
-        label "Mumbai-B2-20.04"
+        label 'Mumbai-A1'
     }
   
   stages{
       
-    stage('Clean workspace'){
-	    steps {
-	        sh """
-            cd $WORKSPACE
-            sudo rm -rf *
-           """
-	    }
-	}
+      stage("Cleanup"){
+      steps {
+        sh """
+        cd $WORKSPACE
+        sudo rm -rf *
+        """
+      }
+    }
 
-   stage('Install aether-onramp') {
+      stage('Install aether-onramp') {
        steps {
            sh """
             cd $WORKSPACE
             git clone --recursive https://github.com/opennetworkinglab/aether-onramp.git 
             cd aether-onramp
-            sudo cp /home/ubuntu/hosts.ini hosts.ini
-            #sudo cp /home/ubuntu/sdcore-5g-values-new.yaml  deps/5gc/roles/core/templates/sdcore-5g-values.yaml
-            sudo cp vars/main-quickstart.yml vars/main.yml
-            grep -rl "ens18" . | xargs sed -i 's/ens18/eth0/g'
+            # determine local IP
+            MYIP=\$(hostname -I | awk '{print \$1}')
+            # determine local NET_INTERFACE (Well, someday...)  
+            # We just know the current Aether-Onramp-Ready AMI is using ens5
+            NET_INTERFACE="eth0"
+            sudo cp -r /home/ubuntu/hosts.ini hosts.ini
+            sudo cp  vars/main-gnbsim.yml  vars/main.yml
             sudo sed -i "s/10.76.28.113/\$(hostname -I | awk '{ print \$1 }')/" vars/main.yml
-            #pipx install --include-deps ansible --force
-            #pipx ensurepath
+            grep -rl "ens18" . | xargs sed -i 's/ens18/eth0/g'
             make aether-pingall
             ansible-galaxy collection install kubernetes.core
-            make k8s-install
             ansible-galaxy collection install ansible.posix
+            make k8s-install
+            kubectl get pods --all-namespaces
             make 5gc-install
-            #ansible-galaxy collection install community.docker:2.7.3 --force
-            #ansible-galaxy collection install ansible.utils:2.8.0 --force
-            #make gnbsim-docker-install
+            ansible-galaxy collection install community.docker:2.7.3 --force
+            ansible-galaxy collection install ansible.utils:2.8.0 --force
+            make gnbsim-docker-install
             make gnbsim-install
-            #make gnbsim-simulator-run
-            #docker exec gnbsim-1 cat summary.log
-            
-           """ 
+            """ 
 	   }
+	  }
+    
+    stage("Run Gnbsim Test"){
+      steps {
+        sh """
+        cd $WORKSPACE/aether-onramp
+        kubectl get pods -n omec
+        sleep 120
+        make gnbsim-simulator-run
+        cd /home/ubuntu
+        ssh -i "aether-qa.pem" ubuntu@172.31.35.35 "docker exec  gnbsim-1 cat summary.log"
+        """
+      }
     }
-    stage('Run GNB-SIM Test'){
-	    steps {
-	        retry(2) {
-	            sh """
-	                cd $WORKSPACE/aether-onramp
-	                sleep 120
-	                kubectl get pods -n omec
-                    make gnbsim-simulator-run
-                    docker exec gnbsim-1 cat summary.log
-            
-            """
-	       } 
-	    }
-	}
-	
-	stage ('GNB-SIM validation'){
-	    steps {
-	        catchError(message:'Gnbsim Validation is failed', buildResult:'FAILURE', stageResult:'FAILURE')
-	        {
-	        sh """
-	        docker exec gnbsim-1 cat summary.log  | grep "Profile Status: PASS"
-	        
-            """
-	        }    
-	    }
-	}
-	
-    stage("Collect GNBSIM and AMF Logs "){
+    
+    stage("Validate GNB-SIM Run Test Results"){
+      steps {
+        catchError(message:'Gnbsim Validation is failed', buildResult:'FAILURE', stageResult:'FAILURE')
+        { 
+        sh """
+        cd /home/ubuntu
+        ssh -i "aether-qa.pem" ubuntu@172.31.35.35 "docker exec  gnbsim-1 cat summary.log" | grep "Profile Status: PASS"  || true
+        """
+        }
+      }
+    }
+    
+   stage("Collect GNBSIM And AMF Logs"){
       steps {
         sh '''
+        cd /home/ubuntu
+        kubectl get pods -n omec
+        logfile1=\$(ssh -i "aether-qa.pem" ubuntu@172.31.35.35 "docker exec  gnbsim-1 ls " | grep "gnbsim1-.*.log")  || true
+        echo "${logfile1}"  || true
+        ssh -i "aether-qa.pem" ubuntu@172.31.35.35 "docker cp gnbsim-1:/gnbsim/bin/${logfile1} /tmp/logs/gnbsim/${logfile1}" || true
+        ssh -i "aether-qa.pem" ubuntu@172.31.35.35 "cat /tmp/logs/gnbsim/${logfile1}" || true
         cd  $WORKSPACE/
 	    mkdir logs
-        kubectl get pods -n omec
-        logfile=\$(docker exec gnbsim-1 ls | grep "gnbsim1-.*.log")
-        echo "${logfile}"
-        docker cp gnbsim-1:/gnbsim/bin/${logfile} logs/${logfile}
-        #cat logs/${logfile}
-	    AMF_POD_NAME=\$(kubectl get pods -n omec | grep amf | awk 'NR==1{print \$1}') 
+        AMF_POD_NAME=\$(kubectl get pods -n omec | grep amf | awk 'NR==1{print \$1}') 
         echo "${AMF_POD_NAME}"
-        kubectl logs $AMF_POD_NAME -n omec > logs/quickstart_2004_default_amf.log
+        kubectl logs $AMF_POD_NAME -n omec > logs/2server_2004_default_amf.log
         WEBUI_POD_NAME=\$(kubectl get pods -n omec | grep webui | awk 'NR==1{print \$1}') 
         echo "${WEBUI_POD_NAME}"
-        kubectl logs $WEBUI_POD_NAME -n omec > logs/quickstart_2004_default_webui.log
+        kubectl logs $WEBUI_POD_NAME -n omec > logs/2server_2004_default_webui.log
         UDR_POD_NAME=\$(kubectl get pods -n omec | grep udr | awk 'NR==1{print \$1}') 
         echo "${UDR_POD_NAME}"
-        kubectl logs $UDR_POD_NAME -n omec > logs/quickstart_2004_default_udr.log
+        kubectl logs $UDR_POD_NAME -n omec > logs/2server_2004_default_udr.log
         UDM_POD_NAME=\$(kubectl get pods -n omec | grep udm | awk 'NR==1{print \$1}') 
         echo "${UDM_POD_NAME}"
-        kubectl logs $UDM_POD_NAME -n omec > logs/quickstart_2004_default_udm.log
+        kubectl logs $UDM_POD_NAME -n omec > logs/2server_2004_default_udm.log
         AUSF_POD_NAME=\$(kubectl get pods -n omec | grep ausf | awk 'NR==1{print \$1}') 
         echo "${AUSF_POD_NAME}"
-        kubectl logs $AUSF_POD_NAME -n omec > logs/quickstart_2004_default_ausf.log
+        kubectl logs $AUSF_POD_NAME -n omec > logs/2server_2004_default_ausf.log
         SMF_POD_NAME=\$(kubectl get pods -n omec | grep smf | awk 'NR==1{print \$1}') 
         echo "${SMF_POD_NAME}"
-        kubectl logs $SMF_POD_NAME -n omec > logs/quickstart_2004_default_smf.log
-            
+        kubectl logs $SMF_POD_NAME -n omec > logs/2server_2004_default_smf.log
+        UPF_POD_NAME=\$(kubectl get pods -n omec | grep upf | awk 'NR==1{print \$1}') 
+        echo "${UPF_POD_NAME}"
+        kubectl logs $UPF_POD_NAME -n omec -c bessd > logs/2server_2004_default_upf.log
+        
         '''
       }
     }
@@ -113,12 +115,13 @@ pipeline {
             archiveArtifacts allowEmptyArchive: true, artifacts: "**/logs/*.log", followSymlinks: false
         }
     }
- }
 
+  }
+  
   post {
       
         always {
-          sh """
+            sh """
             cd $WORKSPACE/aether-onramp
             make monitor-uninstall
             make roc-uninstall
@@ -128,16 +131,12 @@ pipeline {
             make aether-uninstall
            """
                
-           }  
-      
-        // only triggered when blue or green sign
-//            success {
-//                slackSend color: "good", message: "PASSED ${env.JOB_NAME} ${env.BUILD_NUMBER} ${env.BUILD_URL}"           
-//                }
+        }      
         // triggered when red sign
-            failure {
-                slackSend color: "danger", message: "FAILED ${env.JOB_NAME} ${env.BUILD_NUMBER} ${env.BUILD_URL}" 
+        failure {
+            slackSend color: "danger", message: "FAILED ${env.JOB_NAME} ${env.BUILD_NUMBER} ${env.BUILD_URL}" 
 
-           }
-         }
+        }
+    }
+         
 }
