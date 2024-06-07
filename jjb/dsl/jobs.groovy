efc1f5730c5cd46cab6ac1b6218547868cf5cbfd
root <root@ip-172-31-3-143.ap-south-1.compute.internal>
2024-06-07 10:42:09 +0000

    update dsl script

--- jjb/dsl/jobs.groovy
+++ jjb/dsl/jobs.groovy
@@ -1,12 +1,30 @@
-import groovy.yaml.YamlSlurper
+pipeline {
+    agent any
+    stages {
+        stage('Create Jobs') {
+            steps {
+                script {
+                    // Directory containing YAML files for pipeline configurations
+                    def yamlFilesDir = '${WORKSPACE}/jjb/template'
 
-// Function to create pipeline job from YAML configuration
-def createPipelineJobFromYaml(yamlFilePath) {
-    def config = new YamlSlurper().parse(new File(yamlFilePath))
-    def pipelineFile = config['pipeline-script']
-    def description = config['description']
-    def trigger = config['triggers']
-    def jobName = config['name'] ?: getJobNameFromFilename(pipelineFile)
+                    // Read YAML files from the directory
+                    def yamlFiles = findFiles(glob: "${yamlFilesDir}/*.yaml")
+
+                    yamlFiles.each { yamlFile ->
+                        def config = readYaml(file: "${yamlFilesDir}/${yamlFile.name}")
+                        createPipelineJobFromYaml(config)
+                    }
+                }
+            }
+        }
+    }
+}
+
+def createPipelineJobFromYaml(config) {
+    def pipelineFile = config.pipeline-script
+    def description = config.description
+    def trigger = config.triggers
+    def jobName = config.name
     
     def pipelineScript = readFileFromWorkspace(pipelineFile)
     
@@ -23,24 +41,7 @@ def createPipelineJobFromYaml(yamlFilePath) {
     }
 }
 
-// Function to get job name from filename
-def getJobNameFromFilename(filename) {
-    return filename.take(filename.lastIndexOf('.'))
-}
-
-// Function to read file content from workspace
 def readFileFromWorkspace(filePath) {
     def fileContent = readFile(filePath)
     return fileContent
 }
-
-// Directory containing YAML files for pipeline configurations
-def yamlFilesDir = '${WORKSPACE}/jjb/template/'
-
-// Read pipeline configurations from YAML files
-def yamlFiles = new File(yamlFilesDir).listFiles().findAll { it.name.endsWith('.yaml') }
-
-yamlFiles.each { yamlFile ->
-    createPipelineJobFromYaml(yamlFile)
-}
-
